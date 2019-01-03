e791e027e833ec093b4887c502eefe05b59a8394
Sarou <bastien.hiel@gmail.com>
2019-01-03 16:26:00 +0100

    ajout de l'exo_18

---
+++ exo_18.rb
@@ -0,0 +1,23 @@
+puts "Quel est votre prénom (en minuscule)?"
+print ">"
+user_firstname = gets.chomp
+
+puts "Quel est votre nom (en minuscule) ?"
+print ">"
+user_lastname = gets.chomp
+
+puts "Création de 50 emails bidons :"
+count = 01
+myarray = []
+
+while count <= 50 
+	if count < 10
+		emails = "#{user_firstname}.#{user_lastname}.0#{count}@email.fr"
+	else
+		emails = "#{user_firstname}.#{user_lastname}.#{count}@email.fr"
+	end
+	myarray << emails
+	count += 1
+end
+
+print myarray
