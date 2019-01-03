60f7bd5a31fd2aa36bd48f6da657fae1f82075fb
Sarou <bastien.hiel@gmail.com>
2019-01-03 09:40:09 +0100

    ajout exo_07c

---
+++ exo_07c.rb
@@ -0,0 +1,5 @@
+user_name = gets.chomp
+puts user_name
+
+#Dans le deuxième prog on ajoute un chevron dans l'input 
+#Dans le 3ème la question manque donc diffcile pour l'utilisateur de deviner ce qu'il doit faire
